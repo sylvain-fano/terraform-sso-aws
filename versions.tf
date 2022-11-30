355ea7fc51aba53d9e7d844fea6f709e5db42740
Sylvain Fano <sylvain@fano.me>
2022-11-30 17:51:49 +0100

    32 commit

--- versions.tf
+++ versions.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  # access_key = var.aws_access_key_id
+  # secret_key = var.aws_secret_access_key
 }
