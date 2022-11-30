048a471a2dd7ac4c3e535f02979263faffb397b1
Sylvain Fano <sylvain@fano.me>
2022-11-30 16:55:15 +0100

    1st commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region to create resources in"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "permissions_list" {
+  type = list(object({
+    name             = string
+    description      = string
+    session_duration = string
+    managed_policies = list(string)
+    aws_accounts     = list(string)
+    sso_groups       = list(string)
+  }))
+  description = "List of permission set properties"
+}
